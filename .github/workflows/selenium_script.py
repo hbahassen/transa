a29e0815c7350838d3b1dfbeaf734ba49ba6c9c7
hbahassen <38153234+hbahassen@users.noreply.github.com>
2025-02-10 16:56:48 +0100

    selenium_script.py

--- .github/workflows/selenium_script.py
+++ .github/workflows/selenium_script.py
@@ -1,6 +1,7 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Configuration des options de Chrome en mode headless
@@ -11,8 +12,9 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--window-size=1920,1080")
 
-# Initialisation du driver avec webdriver_manager pour installer automatiquement chromedriver
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+# Initialisation du driver avec webdriver_manager et l'objet Service
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=chrome_options)
 
 try:
     # 1. Ouvrir la première URL
